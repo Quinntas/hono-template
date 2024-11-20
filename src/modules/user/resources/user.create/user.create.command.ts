972353264e5c71d3362d70e6c7fb0fc4fd9ac29e
quintas <caioquintassantiago@gmail.com>
2024-11-20 02:17:33 -0300

    fix create user

--- src/modules/user/resources/user.create/user.create.command.ts
+++ src/modules/user/resources/user.create/user.create.command.ts
@@ -1,9 +1,10 @@
 import {createCommand} from "../../../../lib/command.ts";
 import type {UserCreateDTO, UserCreateResponseDTO} from "./user.create.dto.ts";
+import {createUser} from "../../infra/user.repository.ts";
 
 export const userCreateCommand = createCommand<UserCreateDTO, UserCreateResponseDTO>(
     async (dto) => {
-        console.log(dto)
+        await createUser(dto)
 
         return {
             message: 'User created successfully'
